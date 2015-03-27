036077469f7c5c3738d82403eff62938210444d6
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-27 19:54:51 +0000

    Move towards standard style
    Possibly not a full implementation yet, standard doesn't
    support ES6

--- array-like-object-wrapper.js
+++ array-like-object-wrapper.js
@@ -1,65 +1,65 @@
 class ArrayLikeObjectWrapper {
-    constructor(obj) {
-        let specs = {
-            length: {
-                get: function() {
-                    return obj.length;
-                },
-                set: function(val) {
-                    obj.length = val;
-                }
-            }
-        };
-
-        for(let i = 0; i < obj.length; ++i) {
-            specs[i] = {
-                get: function() {
-                    return obj[i];
-                },
-                set: function(val) {
-                    obj[i] = val;
-                },
-                enumerable: true
-            };
+  constructor (obj) {
+    let specs = {
+      length: {
+        get: function () {
+          return obj.length
+        },
+        set: function (val) {
+          obj.length = val
         }
-        Object.defineProperties(this, specs);
+      }
     }
 
-    * keys() {
-        for(let i = 0; i < this.length; ++i) {
-            yield i;
-        }
+    for (let i = 0; i < obj.length; ++i) {
+      specs[i] = {
+        get: function () {
+          return obj[i]
+        },
+        set: function (val) {
+          obj[i] = val
+        },
+        enumerable: true
+      }
     }
+    Object.defineProperties(this, specs)
+  }
 
-    * values() {
-        for(let key of this.keys()) {
-            yield this[key];
-        }
+  * keys () {
+    for(let i = 0; i < this.length; ++i) {
+      yield i
     }
+  }
 
-    * entries() {
-        for(let key of this.keys()) {
-            yield [key, this[key]];
-        }
+  * values () {
+    for(let key of this.keys()) {
+      yield this[key]
     }
+  }
 
-    [Symbol.iterator]() {
-        return this.values();
+  * entries () {
+    for(let key of this.keys()) {
+      yield [key, this[key]]
     }
+  }
+
+  [Symbol.iterator] () {
+    return this.values()
+  }
 }
 
 for(let key of ["every", "copyWithin", "fill", "find", "findIndex", "forEach",
-        "indexOf", "join", "lastIndexOf", "reduce", "reduceRight", "reverse",
-        "some", "sort", "toLocaleString", "toString"]) {
-    ArrayLikeObjectWrapper.prototype[key] = function() {
-        return Array.prototype[key].apply(this, arguments);
-    };
+    "indexOf", "join", "lastIndexOf", "reduce", "reduceRight", "reverse",
+    "some", "sort", "toLocaleString", "toString"]) {
+  ArrayLikeObjectWrapper.prototype[key] = function () {
+    return Array.prototype[key].apply(this, arguments)
+  }
 }
 
 for(let key of ["concat", "filter", "map", "slice"]) {
-    ArrayLikeObjectWrapper.prototype[key] = function() {
-        return this.constructor.from(Array.prototype[key].apply(this, arguments));
-    };
+  ArrayLikeObjectWrapper.prototype[key] = function () {
+    return this.constructor.from(Array.prototype[key].apply(this, arguments))
+  }
 }
 
-export default ArrayLikeObjectWrapper;
+export default ArrayLikeObjectWrapper
