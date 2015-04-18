169699831b0045b236a7b89d987eccd680ab0619
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-18 18:14:44 +0000

    Fix style errors

--- array-like-object-wrapper.js
+++ array-like-object-wrapper.js
@@ -26,19 +26,19 @@ class ArrayLikeObjectWrapper {
   }
 
   * keys () {
-    for(let i = 0; i < this.length; ++i) {
+    for (let i = 0; i < this.length; ++i) {
       yield i
     }
   }
 
   * values () {
-    for(let key of this.keys()) {
+    for (let key of this.keys()) {
       yield this[key]
     }
   }
 
   * entries () {
-    for(let key of this.keys()) {
+    for (let key of this.keys()) {
       yield [key, this[key]]
     }
   }
@@ -48,15 +48,15 @@ class ArrayLikeObjectWrapper {
   }
 }
 
-for(let key of ["every", "copyWithin", "fill", "find", "findIndex", "forEach",
-    "indexOf", "join", "lastIndexOf", "reduce", "reduceRight", "reverse",
-    "some", "sort", "toLocaleString", "toString"]) {
+for (let key of ['every', 'copyWithin', 'fill', 'find', 'findIndex', 'forEach',
+    'indexOf', 'join', 'lastIndexOf', 'reduce', 'reduceRight', 'reverse',
+    'some', 'sort', 'toLocaleString', 'toString']) {
   ArrayLikeObjectWrapper.prototype[key] = function () {
     return Array.prototype[key].apply(this, arguments)
   }
 }
 
-for(let key of ["concat", "filter", "map", "slice"]) {
+for (let key of ['concat', 'filter', 'map', 'slice']) {
   ArrayLikeObjectWrapper.prototype[key] = function () {
     return this.constructor.from(Array.prototype[key].apply(this, arguments))
   }
